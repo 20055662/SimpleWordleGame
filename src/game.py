f9ab693052c695611912ccf764f6de67eac173ed
20055662 <20055662@tafe.wa.edu.au>
2025-09-15 12:40:02 +0800

    refactor:add scoring function to game.py

---
+++ src/game.py
@@ -0,0 +1,18 @@
+# src/game.py
+from utils import load_words
+import random
+
+def score_guess(the_target, the_guess):
+    position = 0
+    symbol = ""
+    for letter in the_guess:
+        if letter == the_target[position]:
+            symbol += "+ "
+        elif letter in the_target:
+            symbol += "? "
+        else:
+            symbol += "_ "
+        position += 1
+    print(*the_guess.upper())
+    print(symbol)
+    return symbol == "+ + + + + "
